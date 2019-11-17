3f019969511c2e25abd5a631c7bc4b7fe9af72e7
matsu-chara <matsuy00@gmail.com>
2019-11-17 15:25:17 +0900

    add sandbox

--- src/main/scala/Sandbox.scala
+++ src/main/scala/Sandbox.scala
@@ -1,8 +1,11 @@
 import cats.effect.{ExitCode, IO, IOApp}
 import fs2.Stream
+import scala.concurrent.duration._
 
 object Sandbox extends IOApp {
   private def put(s: String) = IO(println(s))
+  private def putRange: Stream[IO, Unit] = Stream.range(1, 10).evalMap(x => put(x.toString))
+  private def seconds: Stream[IO, FiniteDuration] = Stream.awakeEvery[IO](1.second)
 
   def hhhw(): Stream[IO, Unit] = {
     Stream.repeatEval(put("hello")).take(3) ++ Stream.eval(put("world"))
@@ -12,7 +15,11 @@ object Sandbox extends IOApp {
     Stream(1,2,3).flatMap(x => Stream.repeatEval(put(x.toString)).take(2))
   }
 
+  def sec(): Stream[IO, (FiniteDuration, Unit)] = {
+    seconds.zip(putRange)
+  }
+
   override def run(args: List[String]): IO[ExitCode] = {
-    hhhw().compile.drain.map(_ => ExitCode.Success)
+    sec().compile.drain.map(_ => ExitCode.Success)
   }
 }
